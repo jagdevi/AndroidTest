0bc82b406d3f834ee11b34be99f2476ee346eb11
JJagdevi <jagdevi@hartleylb.com>
2018-10-27 01:53:46 +0530

    Integrated MapBox api

--- app/src/main/java/com/test/androidtest/CameraActivity.kt
+++ app/src/main/java/com/test/androidtest/CameraActivity.kt
@@ -1,6 +1,7 @@
 package com.test.androidtest
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -9,33 +10,67 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
 import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.File
 import java.io.FileOutputStream
+import java.text.DecimalFormat
 
 class CameraActivity : AppCompatActivity(), View.OnClickListener {
 
+    //MapBox token
+    private val mapAccessToken = "pk.eyJ1IjoiamFnZGV2aSIsImEiOiJjam5wa2RxZWYxdGRkM2twa285dDU3MmN2In0.Knj8iSlFMJdoTDXZc8dc-g"
+    private val PERMISSION_REQUEST_CODE = 101
     private val imageCaptureResult = 1
+    private lateinit var progressDialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
         checkPermissionGranted()
+        progressDialog = showCustomProgressDialog(this)
         btn_capture_image.setOnClickListener(this)
         btn_next.setOnClickListener(this)
-
+        editText_geoTag.setOnClickListener(this)
     }
 
     private fun checkPermissionGranted() : Boolean{
-        return if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+
+        val cameraPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+        val storageWritePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val locationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val fineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        val permissionList = ArrayList<String>()
+        if(cameraPermission != PackageManager.PERMISSION_GRANTED)
+            permissionList.add(Manifest.permission.CAMERA)
+
+        if(storageWritePermission != PackageManager.PERMISSION_GRANTED)
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if(fineLocationPermission != PackageManager.PERMISSION_GRANTED)
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if(locationPermission != PackageManager.PERMISSION_GRANTED)
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        return if(permissionList.isNotEmpty() && permissionList.size > 0){
+            ActivityCompat.requestPermissions(this,permissionList.toTypedArray(), PERMISSION_REQUEST_CODE)
             false
         }else
             true
+
     }
 
     /**
@@ -96,16 +131,97 @@ class CameraActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        when(view?.id){
+        when(view?.id) {
             R.id.btn_capture_image ->
                 takeImageFromCamera()
 
-            R.id.btn_next ->
-                validateInputFields()
+            R.id.btn_next -> {
+                if (validateInputFields())
+                    Log.d("dff","sdf")
+                }
+
+            R.id.editText_geoTag ->
+                initializeMapBox()
         }
     }
 
-    private fun validateInputFields() {
+    /**
+     * function to take users current latitude and longitude
+     */
+    private fun getCurrentLatLong() {
+        val locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine.activate()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val location = locationEngine.lastLocation
+            val latitude = DecimalFormat("##.####").format(location.latitude)
+            val longitude = DecimalFormat("##.####").format(location.longitude)
+            editText_geoTag.setText("$latitude , $longitude")
+            dismissProgressDialog()
+        }
+    }
 
+    private fun initializeMapBox(){
+        progressDialog?.show()
+        Mapbox.getInstance(this,mapAccessToken)
+        if(!checkPermissionGranted())
+            return
+
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            getCurrentLatLong()
+        }else
+            checkPermissionGranted()
+
+    }
+
+    private fun validateInputFields() : Boolean{
+        val objectId = editText_objectId.text?.trim().toString()
+        val latLong = editText_geoTag.text?.trim().toString()
+        val featuredCount = editText_featureCount.text?.trim().toString()
+        return when {
+            objectId.isEmpty() -> {
+                textInput_ObjectId.error = "Enter object id"
+                false
+            }
+            latLong.isEmpty() -> {
+                textInput_ObjectId.isErrorEnabled = false
+                textInput_geoTag.error = "Enter geo tag"
+                false
+            }
+            featuredCount.isEmpty() -> {
+                textInput_geoTag.isErrorEnabled = false
+                textInput_featureCount.error = "Enter feature count"
+                false
+            }
+            else -> {
+                textInput_ObjectId.isErrorEnabled = false
+                textInput_geoTag.isErrorEnabled = false
+                textInput_featureCount.isErrorEnabled = false
+                true
+            }
+        }
+    }
+
+    /**
+     * function to show custom progress dialog
+     */
+    private fun showCustomProgressDialog(context: Context) : AlertDialog {
+        val alertBuilder = AlertDialog.Builder(context)
+        val dialog = LayoutInflater.from(context).inflate(R.layout.custom_progressbar,null)
+        val alertDialog = alertBuilder.create()
+        alertDialog.window.setBackgroundDrawableResource(android.R.color.transparent)
+        alertDialog.setView(dialog)
+        alertDialog.setCanceledOnTouchOutside(false)
+
+        return alertDialog
+    }
+
+    /**
+     * function to dismiss custom progress dialog
+     */
+    private fun dismissProgressDialog() {
+        if(progressDialog.isShowing)
+            progressDialog.dismiss()
     }
 }
